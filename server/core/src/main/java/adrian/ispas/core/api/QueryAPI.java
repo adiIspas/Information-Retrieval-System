1fe72ffd24d8456cd6bc3d84ad2cbd85de815fa5
Adrian Ispas <adrian.ispas@ab4.systems>
2018-03-10 21:14:31 +0200

    Add hello world api endpoint

---
+++ server/core/src/main/java/adrian/ispas/core/api/QueryAPI.java
@@ -0,0 +1,16 @@
+package adrian.ispas.core.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Adrian Ispas on Mar, 2018
+ */
+@RestController
+public class QueryAPI {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+}
