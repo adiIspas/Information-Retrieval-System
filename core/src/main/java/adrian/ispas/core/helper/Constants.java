2d6317544cd58ba7aff58ce81bc90dbacdf69f50
Adrian Ispas <adrian.ispas@ab4.systems>
2018-03-14 23:02:46 +0200

    Add complete files

---
+++ core/src/main/java/adrian/ispas/core/helper/Constants.java
@@ -0,0 +1,14 @@
+package adrian.ispas.core.helper;
+
+/**
+ * Created by Adrian Ispas on Mar, 2018
+ */
+public class Constants {
+    public static final String CONTENTS = "contents";
+    public static final String FILE_NAME = "filename";
+    public static final String FILE_PATH = "filepath";
+    public static final int MAX_SEARCH = 10;
+
+    public static final String DATA_DIR = "/src/main/resources/documents";
+    public static final String INDEX_DIR = "src/main/resources/documents_indexes";
+}
---
+++ core/src/main/java/adrian/ispas/core/retrive/Searcher.java
@@ -0,0 +1,44 @@
+package adrian.ispas.core.retrive;
+
+import adrian.ispas.core.helper.Constants;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+/**
+ * Created by Adrian Ispas on Mar, 2018
+ */
+public class Searcher {
+
+    private IndexSearcher indexSearcher;
+    private QueryParser queryParser;
+    private Query query;
+
+    public Searcher(String indexDirectoryPath) throws IOException {
+        Directory indexDirectory = FSDirectory.open(Paths.get(indexDirectoryPath));
+        DirectoryReader directoryReader = DirectoryReader.open(indexDirectory);
+
+        indexSearcher = new IndexSearcher(directoryReader);
+        queryParser = new QueryParser(Constants.CONTENTS, new StandardAnalyzer());
+    }
+
+    public TopDocs search(String searchQuery) throws ParseException, IOException {
+        query = queryParser.parse(searchQuery);
+        return indexSearcher.search(query, Constants.MAX_SEARCH);
+    }
+
+    public Document getDocument(ScoreDoc scoreDoc) throws IOException {
+        return indexSearcher.doc(scoreDoc.doc);
+    }
+}
